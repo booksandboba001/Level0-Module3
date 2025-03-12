677e49ac11e044b45bc6f70a686f068426735126
booksandboba001 <evie.wuornos@students.jointheleague.org>
2025-03-12 23:47:44 +0000

    worked on Robot Color chooser

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -4,6 +4,8 @@ package _01_else_if._1_robot_color_chooser;
 import javax.swing.JOptionPane;
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
@@ -15,9 +17,72 @@ public class RobotColorChooser {
         //6. If the user doesn't enter anything, choose a random color
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
-
-		
-
+		int numz;
+		Robot randy = new Robot();
+		randy.penDown();
+		randy.setSpeed(100);
+		for (numz =0; numz<4; numz++){
+			randy.move(90);
+			randy.turn(90);
+		}
+		randy.setPenWidth(10);
+		String colore = JOptionPane.showInputDialog("What color do you want the robot to draw with?");
+		if (colore.equals( "brown")){
+			randy.setPenColor(139,69,19);
+		}
+		for (numz =0; numz<4; numz++){
+			randy.move(100);
+			randy.turn(90);
+		}
+		if (colore.equals( "misty rose")){
+			randy.setPenColor(255,228,225);
+		}
+		for (numz =0; numz<4; numz++){
+			randy.move(110);
+			randy.turn(90);
+		}
+		if (colore.equals( "red")){
+			randy.setPenColor(220,20,60);
+		}
+		for (numz =0; numz<4; numz++){
+			randy.move(120);
+			randy.turn(90);
+		}
+		if (colore.equals( "orange")){
+			randy.setPenColor(255,140,0);
+		}
+		for (numz =0; numz<4; numz++){
+			randy.move(130);
+			randy.turn(90);
+		}
+		if (colore.equals( "yellow")){
+			randy.setPenColor(255,215,0);
+		}
+		for (numz =0; numz<4; numz++){
+			randy.move(140);
+			randy.turn(90);
+		}	
+		if (colore.equals( "green")){
+			randy.setPenColor(85,107,47);
+		}
+		for (numz =0; numz<4; numz++){
+			randy.move(150);
+			randy.turn(90);
+		}
+		if (colore.equals( "blue")){
+			randy.setPenColor(0,0,128);
+		}
+		for (numz =0; numz<4; numz++){
+			randy.move(160);
+			randy.turn(90);
+		}
+		if (colore.equals( "purple")){
+			randy.setPenColor(148,0,211);
+		}	
+		for (numz =0; numz<4; numz++){
+			randy.move(170);
+			randy.turn(90);
+		}
 
 
 	}
--- src/_01_else_if/_3_high_low/HighLowGame.java
+++ src/_01_else_if/_3_high_low/HighLowGame.java
@@ -7,8 +7,8 @@ public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
-		
+		int random = new Random().nextInt(100);
+		System.out.println(random);
 		// 2. Print out the random variable above
 		
 		// 11. Repeat steps 1 to 10 ten times
